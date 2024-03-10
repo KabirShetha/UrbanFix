7475cf838dcc131cc1769f1eeae89ed6b123b7c0
KabirShetha <kabirshetha@gmail.com>
2024-03-11 01:23:31 +0530

    check

---
+++ components/IconlyLightProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+const IconlyLightProfile = () => {
+  return (
+    <View>
+      <Image source={require('./path/to/iconly-light-profile.png')} />
+    </View>
+  );
+};
+
+export default IconlyLightProfile;
